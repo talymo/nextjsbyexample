8095483261fadf67356a66c31405e0d8bbe24607
tabethalmoe <101740827+tabethalmoe@users.noreply.github.com>
2023-11-06 13:00:05 -0500

    Added home page

--- next-reviews/app/layout.jsx
+++ next-reviews/app/layout.jsx
@@ -2,8 +2,16 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
            <body>
+            <header>
+                [header]
+            </header>
+            <main>
                 {children}
+            </main> 
+            <footer>
+                [footer]    
+            </footer>               
             </body> 
         </html>
-    )
+    );
 }
